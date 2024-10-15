690ffbf967d3c0353e9fb323a5aa8f301257c4b5
Axtazer <47863741+Axtazer@users.noreply.github.com>
2024-10-15 15:55:37 +0000

    + Desc. Panel

--- pages/panel.php
+++ pages/panel.php
@@ -100,7 +100,10 @@ $activites = getTableData($pdo, 'Activite');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Panel d'administration - <?php echo htmlspecialchars($siteName); ?></title>
+    <meta
+      name="description"
+      content="Panel d'administration de PageBleue.">
+    <title>Panel - <?php echo htmlspecialchars($siteName); ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <style>
