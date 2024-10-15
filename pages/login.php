507ca7ee44fdd0cc538ae0f48620b2219f134c10
Axtazer <47863741+Axtazer@users.noreply.github.com>
2024-10-15 17:48:53 +0200

    Update login.php
    Signed-off-by: Axtazer <47863741+Axtazer@users.noreply.github.com>

--- pages/login.php
+++ pages/login.php
@@ -117,6 +117,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta
+      name="description"
+      content="Page de connexion à PageBleue.">
     <title>Connexion - <?php echo htmlspecialchars($siteName); ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
