fa66b0ed0c4a3fdc2c5a0d2270e4eb9a6dd17132
Axtazer <47863741+Axtazer@users.noreply.github.com>
2024-10-17 21:45:39 +0200

    Retrait css + font déjà mis

--- pages/form.php
+++ pages/form.php
@@ -13,8 +13,6 @@ includeWidget('footer');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo htmlspecialchars($siteName); ?> - Formulaire</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <meta
       name="description"
       content="PageBleue, page de formulaire pour l'ajout d'entreprises à la liste de PageBleue.">
