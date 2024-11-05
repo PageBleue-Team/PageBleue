70f596ac9d0e52b0376c4b0ba249a2262f0de63d
Axtazer <47863741+Axtazer@users.noreply.github.com>
2024-11-06 00:59:38 +0100

    Changement nom du dossier

--- config/init.php
+++ config/init.php
@@ -1,6 +1,6 @@
 <?php
 // Chargement des fichiers de configuration dans le bon ordre
-require_once __DIR__ . '/package/paths.php';
+require_once __DIR__ . '/Package/paths.php';
 require_once ROOT_PATH . '/vendor/autoload.php';
 
 // Chargement des variables d'environnement
@@ -11,8 +11,8 @@ $dotenv->load();
 session_start();
 
 // Chargement des autres configurations
-require_once __DIR__ . '/package/database.php';
-require_once __DIR__ . '/package/cache.php';
-require_once __DIR__ . '/package/security.php';
-require_once __DIR__ . '/package/site.php';
-require_once __DIR__ . '/package/utils.php';
+require_once __DIR__ . '/Package/database.php';
+require_once __DIR__ . '/Package/cache.php';
+require_once __DIR__ . '/Package/security.php';
+require_once __DIR__ . '/Package/site.php';
+require_once __DIR__ . '/Package/utils.php';
