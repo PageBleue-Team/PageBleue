f5f374845eb10408150994d8784428b618bfc706
Axtazer <47863741+Axtazer@users.noreply.github.com>
2024-11-09 00:18:27 +0100

    Ajout de paths en plus

--- config/Package/paths.php
+++ config/Package/paths.php
@@ -1,6 +1,6 @@
 <?php
 
-// Définition des chemins absolus
+// Définition des chemins absolus (système de fichiers)
 define('ROOT_PATH', dirname(dirname(__DIR__)));
 define('CONFIG_PATH', ROOT_PATH . '/config');
 define('TEMPLATES_DIR', ROOT_PATH . '/templates');
@@ -10,3 +10,10 @@ define('LOGO_PATH', ASSETS_PATH . '/images/logos');
 define('VAR_PATH', ROOT_PATH . '/var');
 define('CACHE_PATH', VAR_PATH . '/cache');
 define('LOGS_PATH', VAR_PATH . '/logs');
+
+// Définition des URLs (chemins web)
+define('ASSETS_URL', '/assets');
+define('JS_URL', ASSETS_URL . '/js');
+define('CSS_URL', ASSETS_URL . '/css');
+define('IMAGES_URL', ASSETS_URL . '/images');
+define('LOGOS_URL', IMAGES_URL . '/logos');
